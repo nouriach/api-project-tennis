8b9f3f886cc3367a7e81add72338d6da73363eee
NOURIACH <NOURIACH@LTWT618415>
2021-01-06 16:10:27 +0000

    Add Create method to Controller

--- Tennis.Data.Api.Web/Controllers/PlayersController.cs
+++ Tennis.Data.Api.Web/Controllers/PlayersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tennis.Data.Api.Domain.Contracts;
 using Tennis.Data.Api.Domain.Models;
 
 namespace Tennis.Data.Api.Web.Controllers
@@ -21,10 +22,26 @@ namespace Tennis.Data.Api.Web.Controllers
             }
         }
 
-        [HttpGet("api/v1/players")]
+        [HttpGet(ApiRoutes.Players.GetAll)]
         public IActionResult GetAll()
         {
             return Ok(_players);
         }
+
+        [HttpPost(ApiRoutes.Players.Create)]
+        public IActionResult Create([FromBody] Player player)
+        {
+            if (!string.IsNullOrEmpty(player.FirstName))
+            {
+                player.FirstName = Guid.NewGuid().ToString();
+            }
+            _players.Add(player);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Players.Get.Replace("{playerId}", player.FirstName);
+
+            return Created(locationUri, player);
+        }
+
     }
 }
