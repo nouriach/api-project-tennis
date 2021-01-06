5bbecc65a61042215e86e7b14cfd11ed5cf8bfbb
NOURIACH <NOURIACH@LTWT618415>
2021-01-06 17:14:00 +0000

    Build simple CreateCommand & Result

---
+++ Tennis.Data.Api.Domain/Models/Players/Commands/CommandQueries/CreatePlayerCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tennis.Data.Api.Domain.Models.Players.Commands.CommandResults;
+
+namespace Tennis.Data.Api.Domain.Models.Players.Queries
+{
+    public class CreatePlayerCommand : IRequest<CreatePlayerCommandResult>
+    {
+        public string FirstName { get; set; }
+    }
+}
---
+++ Tennis.Data.Api.Domain/Models/Players/Commands/CommandResults/CreatePlayerCommandResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tennis.Data.Api.Domain.Models.Players.Commands.CommandResults
+{
+    public class CreatePlayerCommandResult
+    {
+        //public Player Player { get; set; }
+        public string FirstName { get; set; }
+    }
+}
