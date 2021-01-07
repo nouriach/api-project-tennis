adcd4ee609dd7356f4c1fd179f9661cb14b33c4c
NOURIACH <NOURIACH@LTWT618415>
2021-01-07 17:38:17 +0000

    Move all Queries to Application project

---
+++ Tennis.Data.Api.Application/Players/Handlers/GetAllPlayersQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tennis.Data.Api.Application.Players.Queries;
+using Tennis.Data.Api.Domain.Models;
+
+namespace Tennis.Data.Api.Application.Players.Handlers
+{
+    class GetAllPlayersQueryHandler : IRequestHandler<GetAllPlayersQuery, IEnumerable<Player>>
+    {
+        public Task<IEnumerable<Player>> Handle(GetAllPlayersQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tennis.Data.Api.Application/Players/Handlers/GetPlayerQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tennis.Data.Api.Application.Players.Queries;
+using Tennis.Data.Api.Domain.Models;
+
+namespace Tennis.Data.Api.Application.Players.Handlers
+{
+    class GetPlayerQueryHandler : IRequestHandler<GetPlayerQuery, Player>
+    {
+        public Task<Player> Handle(GetPlayerQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
