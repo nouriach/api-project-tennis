fcafd4dbbcc1dbe070dea010f425e31dcf67d35d
NOURIACH <NOURIACH@LTWT618415>
2021-01-07 08:19:21 +0000

    Extract & organise classes ready to migrate into a DB

--- Tennis.Data.Api.Domain/Models/Player.cs
+++ Tennis.Data.Api.Domain/Models/Player.cs
@@ -27,34 +27,4 @@ namespace Tennis.Data.Api.Domain.Models
         public Skill Skill { get; set; }
         public Style Style { get; set; }
     }
-    public class Skill
-    {
-        public int Speed { get; set; }
-        public int Serve { get; set; }
-        public int Power { get; set; }
-        public int Technique { get; set; }
-        public int Endurance { get; set; }
-        public int Flair { get; set; }
-    }
-
-    public class Style
-    {
-        [JsonProperty("hard_hitter")]
-        public int HardHitter { get; set; }
-
-        [JsonProperty("tactical")]
-        public int Tactical { get; set; }
-
-        [JsonProperty("serve_and_volley")]
-        public int ServeAndVolley { get; set; }
-
-        [JsonProperty("great_return")]
-        public int GreatReturn { get; set; }
-
-        [JsonProperty("rocket_serve")]
-        public int RocketServe { get; set; }
-
-        [JsonProperty("solid_defence")]
-        public int SolidDefence { get; set; }
-    }
 }
