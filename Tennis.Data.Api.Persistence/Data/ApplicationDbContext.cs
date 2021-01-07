6da68d87349e5965f3feb5ae123b149195c8f131
NOURIACH <NOURIACH@LTWT618415>
2021-01-07 17:40:08 +0000

    Add Database files to Persistence

---
+++ Tennis.Data.Api.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tennis.Data.Api.Domain.Models;
+
+namespace Tennis.Data.Api.Persistence.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Skill> Skils { get; set; }
+        public DbSet<Style> Styles { get; set; }
+
+    }
+}
