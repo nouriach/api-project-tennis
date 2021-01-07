6c59bb7f74cc354e22ce412e2b649d1299bc42d1
NOURIACH <NOURIACH@LTWT618415>
2021-01-07 17:39:30 +0000

    Move required files to Infrastructure

---
+++ Tennis.Data.Api.Infrastructure/Options/Installers/DataInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tennis.Data.Api.Application.Interfaces;
+using Tennis.Data.Api.Application.Players.Services;
+using Tennis.Data.Api.Persistence.Data;
+
+
+namespace Tennis.Data.Api.Infrastructure.Options.Installers
+{
+    public class DataInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddSingleton<IPlayerService, PlayerService>();
+
+        }
+    }
+}
---
+++ Tennis.Data.Api.Infrastructure/Options/Installers/MvcInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tennis.Data.Api.Infrastructure.Options.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Tennis Data API", Version = "v1" });
+            });
+        }
+    }
+}
