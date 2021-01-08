2b69c36486e771d915cfb4135ad35b4b9eb98cfc
NOURIACH <NOURIACH@LTWT618415>
2021-01-08 12:10:32 +0000

    Create DbSeeder class to fill database with mock data

---
+++ Tennis.Data.Api.Persistence/Data/DbSeeder.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tennis.Data.Api.Domain.Models;
+
+namespace Tennis.Data.Api.Persistence.Data
+{
+    public static class DbSeeder
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+            JsonSerializer serializer = new JsonSerializer();
+            Player player;
+            Players players;
+
+
+        }
+    }
+}
